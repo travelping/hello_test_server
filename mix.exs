16525320a2b2133cb36a31e7d917ef62a2420519
Dima Aleksandrov <elixmoon@gmail.com>
2015-07-23 15:34:37 +0200

    add metricman to dependencies, add release enviroments

--- mix.exs
+++ mix.exs
@@ -3,17 +3,17 @@ defmodule HelloTestServer.Mixfile do
 
   def project do
     [app: :hello_test_server,
-     version: "0.0.1",
+     version: "0.1.0",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps]
+     deps: deps(Mix.env)]
   end
 
   # Configuration for the OTP application
   #
   # Type `mix help compile.app` for more information
   def application do
-    [applications: [:logger, :elixir, :hello],
+    [applications: [:logger, :elixir, :hello, :runtime_tools, :exrun | (if Mix.env == :release do [:lager_journald_backend] else [] end)],
      mod: {HelloTestServer, []}]
   end
 
@@ -26,8 +26,18 @@ defmodule HelloTestServer.Mixfile do
   #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
   #
   # Type `mix help deps` for more examples and options
-  defp deps do
+  @doc_deps [:earmark, :ex_doc]
+  defp deps(:release) do
+    Code.eval_file("mix.lock")
+    |> elem(0)
+    |> Enum.filter_map(&(not (&1 in @doc_deps)), fn({key, _}) -> {key, path: "deps/" <> "#{key}", override: true} end)
+  end
+
+  defp deps(_) do
     [{:lager, "~> 2.1.1", override: true},
+     {:exrun, github: "liveforeverx/exrun"},
+     {:exrm, "~> 0.18.0"},
+     {:metricman, github: "surik/metricman"},
      {:jsx, github: "liveforeverx/jsx", branch: "mix_compile", override: true},
      {:hello, github: "travelping/hello", branch: "master"}]
   end
